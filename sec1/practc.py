1b40a94fd67594b3e8545b112dc867d0919acf97
ayoub26666666 <153772960+ayoub26666666@users.noreply.github.com>
2025-07-28 18:31:04 +0100

    first section

--- sec1/practc.py
+++ sec1/practc.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 
 import random
 
 print("Hello this is your handgame program!!")
@@ -14,7 +14,7 @@ game_over = False
 
 while not game_over:
     choice = str(input("Enter your guess letter: ")).lower()
-    for position in range(len(secret)):
+    for position in range(len(secret)): # loop through each letter in the secret word
         letter = secret[position]
         if choice == letter :
             empty[position]= letter
